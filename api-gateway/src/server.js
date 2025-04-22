fec15596682a8b562bbc4d1ecf64e5cc8de2d8af
Charles <adeakinyemi129@gmail.com>
2025-04-22 09:58:44 +0100

    finished proxy implementation

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -7,6 +7,7 @@ const {rateLimit} = require('express-rate-limit')
 const {RedisStore} = require('rate-limit-redis')
 const logger = require('./utils/logger.js')
 const proxy = require('express-http-proxy')
+const errorHandler = require('./middleware/errorHandler.js')
 
 
 const app = express()
@@ -21,14 +22,14 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 
-app.use((req, res)=>{
+app.use((req, res, next)=>{
     logger.info(`Recieved ${req.method} request to ${req.url}`);
-    logger.info(`Request body: ${req.body}`)
+    logger.info(`Request body: ${req.body}`);
     next()
 })
 
 
-const endpointrateLimit = new rateLimit({
+const endpointrateLimit = rateLimit({
     windowMs: 15*60*1000, // 15 minutes
     max: 50, //50 request in 15 minutes
     standardHeaders: true,
@@ -73,5 +74,13 @@ app.use('/v1/auth', proxy(process.env.IDENTITY_SERVICE_URL, {
         return proxyResData
     }
 }))
+
+app.use(errorHandler)
+
+app.listen(port, ()=>{
+    logger.info(`Api gateway service is running on port ${port}`)
+    logger.info(`Identity service is running on port ${process.env.IDENTITY_SERVICE_URL}`)
+    logger.info(`Redis url ${process.env.REDIS_URL}`)
+})
 
 
